c91249895ddd050980aeb5b3d617cd87f6322626
Revuubot <aayusha766@gmail.com>
2026-03-29 21:29:34 +0530

    Fix Vercel 500 deployment error by using absolute paths and modern configuration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -12,8 +13,11 @@ app = FastAPI(
 
 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+# Get the absolute path of the current directory
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+app.mount("/static", StaticFiles(directory=os.path.join(BASE_DIR, "static")), name="static")
+templates = Jinja2Templates(directory=os.path.join(BASE_DIR, "templates"))
 
 
 
